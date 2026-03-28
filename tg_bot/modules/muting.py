95cf6c53a5322e65946e7721e09962d324b04bfd
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-03-28 20:06:21 +0530

    Update muting.py

--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -178,10 +178,10 @@ def temp_mute(bot: Bot, update: Update, args: List[str]) -> str:
 
 
 __help__ = """
-*Admin only:*
- - /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
- - /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
- - /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
+❖ *Admin only:*
+ ➻ /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
+ ➻ /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
+ ➻ /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
 """
 
 __mod_name__ = "Muting"
