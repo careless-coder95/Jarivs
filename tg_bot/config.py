6cc980a42d14a019d07ac52ef38b0002c60d5c9d
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-03-28 18:13:07 +0530

    Create config.py

---
+++ tg_bot/config.py
@@ -0,0 +1,11 @@
+from tg_bot.sample_config import Config
+class Development(Config):
+    API_KEY = "YOUR_BOT_TOKEN"
+    OWNER_ID = 6567187829
+    OWNER_USERNAME = "USERNAME"
+    SQLALCHEMY_DATABASE_URI = "DATABASE_URI"
+    MESSAGE_DUMP = -1003764887290
+    SUDO_USERS = []
+    SUDO_USERS = []
+    SUPPORT_USERS = []
+    WHITELIST_USERS = []
